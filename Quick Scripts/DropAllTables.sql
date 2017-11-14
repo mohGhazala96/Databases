f849ac063c4a9cf9009c17daf24d49ef6c87ae6e
Karim El Ghandour <kghandour@live.com>
2017-11-14 21:48:02 +0200

    Quick Scripts

---
+++ Quick Scripts/DropAllTables.sql
@@ -0,0 +1,8 @@
+EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+DECLARE @sql NVARCHAR(max)=''
+
+SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
+FROM   INFORMATION_SCHEMA.TABLES
+WHERE  TABLE_TYPE = 'BASE TABLE'
+
+Exec Sp_executesql @sql

3427d699a8b797425dcc0ff3a660697d7b42ce97
Karim El Ghandour <kghandour@live.com>
2017-11-14 17:11:48 +0200

    All working

--- Manager_To_HR_Employee_apply_replace_Request.sql
+++ Manager_To_HR_Employee_apply_replace_Request.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Managers(
-    username VARCHAR (50),
+    username VARCHAR (20),
     type VARCHAR (50) NOT NULL,
 
     PRIMARY KEY (username),
@@ -9,23 +9,23 @@ CREATE TABLE Managers(
 CREATE TABLE Announcements(
     date DATETIME,
     title VARCHAR(50),
-    hr_employee VARCHAR (50),
+    hr_employee VARCHAR (20),
     type VARCHAR (50) NOT NULL,
     description VARCHAR(MAX) NOT NULL, 
 
     PRIMARY KEY (date, title, hr_employee),
-    FOREIGN KEY (hr_employee) REFERENCES HR_Employees (username) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (hr_employee) REFERENCES HR_Employees (username) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 CREATE TABLE Requests(
     start_date DATETIME,
-    applicant VARCHAR(50),
+    applicant VARCHAR(20),
     end_date DATETIME NOT NULL,
     request_date DATETIME NOT NULL,
     total_days AS end_date - start_date,
-    hr_employee VARCHAR(50),
+    hr_employee VARCHAR(20),
     hr_response VARCHAR(50) NOT NULL,
-    manager VARCHAR (50),
+    manager VARCHAR (20),
     manager_response VARCHAR(50) NOT NULL,
     manager_reason VARCHAR(MAX),
 
@@ -34,13 +34,13 @@ CREATE TABLE Requests(
 
     PRIMARY KEY (start_date, applicant),
     FOREIGN KEY (applicant) REFERENCES Staff_Members (username) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (hr_employee) REFERENCES HR_Employees (username) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (manager)   REFERENCES Managers (username) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (hr_employee) REFERENCES HR_Employees (username) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (manager) REFERENCES Managers (username) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
 
 CREATE TABLE Leave_Requests(
     start_date DATETIME,
-    applicant VARCHAR(50),
+    applicant VARCHAR(20),
 
     PRIMARY KEY (start_date, applicant),
     FOREIGN KEY (start_date, applicant) REFERENCES Requests (start_date, applicant) ON DELETE CASCADE ON UPDATE CASCADE
@@ -48,7 +48,7 @@ CREATE TABLE Leave_Requests(
 
 CREATE TABLE Business_Trip_Requests(
     start_date DATETIME,
-    applicant VARCHAR(50),
+    applicant VARCHAR(20),
     destination VARCHAR(50) NOT NULL,
     purpose VARCHAR(1000) NOT NULL,
 
@@ -58,12 +58,12 @@ CREATE TABLE Business_Trip_Requests(
 
 CREATE TABLE HR_Employee_apply_replace_Requests(
     start_date DATETIME,
-    applicant VARCHAR(50),
-    hr_employee VARCHAR(50),
-    replacement VARCHAR(50) NOT NULL,
+    applicant VARCHAR(20),
+    hr_employee VARCHAR(20),
+    replacement VARCHAR(20) NOT NULL,
 
     PRIMARY KEY (start_date, applicant),
     FOREIGN KEY (start_date, applicant) REFERENCES  Requests (start_date, applicant) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (hr_employee) REFERENCES  HR_Employees (username) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (replacement) REFERENCES  HR_Employees (username) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (hr_employee) REFERENCES  HR_Employees (username) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (replacement) REFERENCES  HR_Employees (username) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) 

743d1f19483124ef344b0bc6c2458bc696a3ffc7
mohGhazala96 <mohamed.aboughazala@student.guc.edu.eg>
2017-12-08 16:01:48 +0200

    added creation for project

---
+++ iWork/iWork/manager/Create_Projects_Manager.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" Inherits="iWork.Create_Projects_Manager" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>Create_Projects_Manager</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+        <asp:Label id="project_name" runat="server" Text="Project Name"></asp:Label><br />
+        <asp:TextBox id="project_name_input" runat="server"></asp:TextBox><br/> 
+          <asp:Label id="startDate" runat="server" Text=" Start date"></asp:Label><br />
+        <asp:TextBox id="startDate_input" runat="server"></asp:TextBox><br/> 
+         <asp:Label id="endDate" runat="server" Text="End date"></asp:Label><br />
+        <asp:TextBox id="endDate_input" runat="server"></asp:TextBox><br/> 
+        <asp:Button id="Btn_CreateProject" runat="server" Text="Create Project" OnClick="createProject"></asp:Button>
+    </form>
+</body>
+</html>
---
+++ iWork/iWork/manager/Create_Projects_Manager.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace iWork
+{
+
+    public partial class Create_Projects_Manager : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["MyDbConn"].ToString();
+
+        protected void createProject(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+
+
+            SqlCommand cmd = new SqlCommand("Create_project", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@manager_name", Session["Username"].ToString()));
+            cmd.Parameters.Add(new SqlParameter("@name_in", project_name_input.Text));
+
+            try
+            {
+                cmd.Parameters.Add(new SqlParameter("@start_date_in", DateTime.Parse(startDate_input.Text)));
+                cmd.Parameters.Add(new SqlParameter("@end_date_in", DateTime.Parse(endDate_input.Text))); 
+                if (project_name_input.Text.Length == 0 || startDate_input.Text.Length == 0 || endDate_input.Text.Length == 0)
+                {
+
+
+                    Response.Write("Fill all inputs");
+
+                }
+                else
+                {
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    Response.Write("Succeeded");
+                    Response.Redirect("Create_Projects_Manager.aspx", true);
+
+
+                }
+            }
+            catch
+            {
+                Response.Write("Write the date in a valid form");
+
+            }
+
+         
+
+        
+        }
+    }
+}

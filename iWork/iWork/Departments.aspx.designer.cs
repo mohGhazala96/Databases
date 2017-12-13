40451bd4db44a705804f63610f333e7ad9c8bfd0
mohGhazala96 <mohamed.aboughazala@student.guc.edu.eg>
2017-12-13 06:55:56 +0200

    adding 3 mand

---
+++ iWork/iWork/Departments.aspx.designer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace SampleWebsite
+{
+
+    public partial class Departments
+    {
+        protected System.Web.UI.HtmlControls.HtmlForm form1;
+
+
+    }
+}
